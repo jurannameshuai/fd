7ee33960ba29af2546402344c09963478c69369e
娄梦宁 <mengning.lou@taojiji.com>
2019-01-29 14:32:39 +0800

    测试

---
+++ application/admin/lang/zh-cn/article.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'Id'  =>  '自增主键',
+    'Title'  =>  '标题',
+    'Createtime'  =>  '创建时间',
+    'Updatetime'  =>  '最后更新时间',
+    'Maincontent'  =>  '内容',
+    'Create_user_id'  =>  '创建人',
+    'Update_user_id'  =>  '最后更新人',
+    'Description'  =>  '描述'
+];
---
+++ application/admin/lang/zh-cn/student/card.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'Name'  =>  '姓名',
+    'Sex'  =>  '性别',
+    'Nation'  =>  '民族',
+    'Born'  =>  '出生年月日',
+    'Address'  =>  '身份证地址',
+    'Card_no'  =>  '身份证号',
+    'User_lifeb'  =>  '身份证有效期起始日期',
+    'User_lifee'  =>  '身份证有效期结束日期',
+    'Police'  =>  '注册地警察局',
+    'Photo'  =>  '证件图片',
+    'Front_img'  =>  '证件正面图',
+    'Back_img'  =>  '证件反面图'
+];
